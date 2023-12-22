12c99050054794521c0ac93ab9f0a2aaede33e0c
sickk <jaakmyr@gmail.com>
2023-12-22 00:18:59 -0800

    okay guys i updated it and its pretty freaking cool

--- js/main.js
+++ js/main.js
@@ -7,12 +7,24 @@ function fetchRepos() {
     .then(response => response.json())
     .then(data => {
         const repoList = document.getElementById('repo-list');
+        // Sort repositories by creation date, newest first
+        data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
         data.forEach(repo => {
             const listItem = document.createElement('li');
             const link = document.createElement('a');
             link.href = repo.html_url;
             link.textContent = repo.name;
             listItem.appendChild(link);
+
+            // Check if the repository has GitHub Pages enabled
+            if (repo.has_pages) {
+                const pagesLink = document.createElement('a');
+                pagesLink.href = `https://${repo.owner.login}.github.io/${repo.name}`;
+                pagesLink.textContent = ' [GitHub Page]';
+                pagesLink.style.marginLeft = '10px';
+                listItem.appendChild(pagesLink);
+            }
+
             repoList.appendChild(listItem);
         });
     })
