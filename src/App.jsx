159c034754c4bf8e84ce0853a66063329fcc046b
sickk <jaakmyr@gmail.com>
2024-10-12 17:15:55 -0700

    removed scroll section

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,6 @@ function App() {
     <Header HeaderText={"sickkmade"}/>
     <section className="app--scrollsection">
     <ThinCenterBar>
-      <ObserverText innerText={"Below Are some of my projects"}/>
-      <ObserverText innerText={"Scroll down and click on the github pages button to see"}/>
-      <ObserverText innerText={"Click on my name at the top for my github profile"}/>
-      <ObserverText innerText={"This is a test for text scrolling in"}/>
-      <ObserverText innerText={"Okay im done :P"}/>
     </ThinCenterBar>
     </section>
     <CardSection />
